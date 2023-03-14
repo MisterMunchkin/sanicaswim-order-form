2a840ff875055e09b912482c2deb07d27648bee3
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-03-14 10:57:21 +0800

    added montserrat globally and tested it with <p>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import './globals.css'
+import { Montserrat } from 'next/font/google';
 
 export const metadata = {
   title: 'SanicaSwim Order Form',
   description: 'Order your bikinies now!',
 }
+
+const montserrat = Montserrat({ 
+  subsets: ['latin'],
+  variable: '--font-montserrat'
+})
 
 export default function RootLayout({
   children,
@@ -11,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${montserrat.variable} font-sans`}>
       <body>{children}</body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-montserrat)', ...fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
