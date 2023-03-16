1439cc9283af916a7ed396bea0fcf47779a224c8
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-03-16 13:46:49 +0800

    updated text of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <main className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
       <div className="w-full max-w-md space-y-8">
         <div>
-          <h2 className='mt-6 text-center text-3xl font-bold tracking-tight'>Sanica Swim Order Form</h2>
-          <p className='mt-2 text-center text-sm'>
-            Please fill out the form below! After you submit the form, <a target="_blank" href="https://www.instagram.com/sanicaswim/" className="text-ss-blue underline decoration-wavy underline-offset-4 hover:underline hover:decoration-wavy sm:no-underline">@SanicaSwim</a> will DM to confirm your order!
+          <h2 className='mt-6 text-center text-3xl font-bold tracking-tight pb-8'>Order Form</h2>
+          <p className='text-center text-sm leading-6'>
+            <a target="_blank" href="https://www.instagram.com/sanicaswim/" className="text-ss-blue underline decoration-wavy underline-offset-4 hover:underline hover:decoration-wavy sm:no-underline">@SanicaSwim</a> will DM to confirm your order after submission
           </p>
         </div>
 
