e769107536abfb3e26f0f55c21480242d0bb04ac
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-03-16 23:18:45 +0800

    phone number works, only needs UI enhancements now. - also added a submit button - removed proof of payment as that is not in the scope anymore - removed orders for now, I'll deal with hell later.

--- src/components/order-form.tsx
+++ src/components/order-form.tsx
@@ -5,19 +5,22 @@ import axios from "axios"
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import "yup-phone-lite";
+
 import { SizeTypes } from '../enums/size';
 
 const orderSchema = yup.object().shape({
   name: yup.string().required(),
   size: yup.mixed<SizeTypes>().oneOf(Object.values(SizeTypes)).required(),
-  price: yup.number().positive('Must be more than 0').required()
+  quantity: yup.number().positive('Must be more than 0').required()
 });
 
 const orderFormSchema = yup.object().shape({
-  instagramLink: yup.string().url().required(),
-  orders: yup.array().of(yup.string()).min(1).required(),
-  address: yup.string().required(),
-  proofOfPayment: yup.mixed().required()
+  instagramLink: yup.string().url().required("Instagram link is required"),
+  fullName: yup.string().required("Full name is required"),
+  phoneNumber: yup.string().phone("PH", "Invalid phone number").required("Phone number is required"),
+  // orders: yup.array().of(yup.string()).min(1).required("Atleast 1 order is required"),
+  address: yup.string().required("Address is required"),
 });
 
 export default function OrderForm() {
@@ -37,14 +40,9 @@ export default function OrderForm() {
     setSubmitting(true);
     setSubmitError(null);
 
-    const formData = new FormData();
-    formData.append("instagramLink", data.instagramLink);
-    formData.append("orders", data.orders.join(","));
-    formData.append("address", data.address);
-    formData.append("proofOfPayment", data.proofOfPayment[0]);
-
     try {
-      await axios.post("/api/orders", formData);
+      // await axios.post("/api/orders", formData);
+      console.log(data);
       setSubmitSuccess(true);
     } catch (error: any) {
       setSubmitError(error);
@@ -67,7 +65,7 @@ export default function OrderForm() {
                   <input
                     type="text"
                     id="instagramLink"
-                    className={`${errors.instagramLink ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6`}
+                    className={`${errors.instagramLink ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-ss-pink sm:text-sm sm:leading-6`}
                     placeholder="Enter Instagram link"
                     {...register("instagramLink")}
                   />
@@ -77,20 +75,34 @@ export default function OrderForm() {
                 </div>
 
                 <div className="col-span-3">
-                  <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="orders">
-                    Orders
+                  <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="fullName">
+                    Full Name
                   </label>
-                  <select
-                    id="orders"
-                    className={`${errors.orders ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6`}
-                    {...register("orders")}
-                  >
-                    <option value="Product A">Product A</option>
-                    <option value="Product B">Product B</option>
-                    <option value="Product C">Product C</option>
-                  </select>
-                  {errors.orders && (
-                    <p className="text-red-500 mt-1">{typeof errors.orders.message === 'string' ? errors.orders.message : 'Invalid input'}</p>
+                  <input
+                    type="text"
+                    id="fullName"
+                    className={`${errors.fullName ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-ss-pink sm:text-sm sm:leading-6`}
+                    placeholder="Enter Full name"
+                    {...register("fullName")}
+                  />
+                  {errors.fullName && (
+                    <p className="text-red-500 mt-1">{typeof errors.fullName.message === 'string' ? errors.fullName.message : 'Invalid input'}</p>
+                  )}
+                </div>
+                
+                <div className="col-span-3">
+                  <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="phoneNumber">
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    id="phoneNumber"
+                    className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-ss-pink sm:text-sm sm:leading-6"
+                    placeholder="Enter Phone Number"
+                    {...register("phoneNumber")}
+                  />
+                  {errors.phoneNumber && (
+                    <p className="text-red-500 mt-1">{typeof errors.phoneNumber.message === 'string' ? errors.phoneNumber.message : 'Invalid input'}</p>
                   )}
                 </div>
 
@@ -101,13 +113,40 @@ export default function OrderForm() {
                   <textarea
                     id="address"
                     rows={3}
-                    className={`${errors.address ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6`}
+                    className={`${errors.address ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-ss-pink sm:text-sm sm:leading-6`}
                     placeholder="Enter delivery address"
                     {...register("address")}
                   ></textarea>
                   {errors.address && (
                     <p className="text-red-500 mt-1">{typeof errors.address.message === 'string' ? errors.address.message : 'Invalid input'}</p>
                   )}
+                </div>
+
+                {/* <div className="col-span-3">
+                  <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="orders">
+                    Orders
+                  </label>
+                  <select
+                    id="orders"
+                    className={`${errors.orders ? "border-red-500" : ""} mt-2 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-ss-pink sm:text-sm sm:leading-6`}
+                    {...register("orders")}
+                  >
+                    <option value="Product A">Product A</option>
+                    <option value="Product B">Product B</option>
+                    <option value="Product C">Product C</option>
+                  </select>
+                  {errors.orders && (
+                    <p className="text-red-500 mt-1">{typeof errors.orders.message === 'string' ? errors.orders.message : 'Invalid input'}</p>
+                  )}
+                </div> */}
+
+                <div className="col-start-2 col-span-1 place-content-center">
+                  <button
+                    type="submit"
+                    className="inline-flex justify-center w-full py-2 px-3 rounded-md bg-ss-blue text-sm font-semibold text-ss-pink shadow-sm hover:bg-ss-pink hover:text-ss-blue"
+                  >
+                    Submit
+                  </button>
                 </div>
               </div>
             </div>
