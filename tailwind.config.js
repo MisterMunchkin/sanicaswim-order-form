4a97417a4403e344bfdd7306c4b54bae27a887bc
Robin Dalmy Tubungbanua <MisterMunchkin@users.noreply.github.com>
2023-04-01 22:56:30 +0800

    fixed random font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-montserrat)', ...fontFamily.sans]
+        sans: ['var(--font-montserrat)']
       },
       colors: {
         'ss-blue': '#477287',
